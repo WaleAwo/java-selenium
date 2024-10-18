1b147c2bade06e84ceee0ede5c260b1759bf6c28
Wale <56082759+WaleAwo@users.noreply.github.com>
2024-10-18 10:01:55 +0100

    moved screenshot method to utility class

---
+++ src/main/java/utilities/ScreenshotUtility.java
@@ -0,0 +1,30 @@
+package utilities;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ScreenshotUtility {
+    public static void takeFailedResultScreenshot(WebDriver driver, ITestResult testResult) {
+        if (ITestResult.FAILURE == testResult.getStatus()) {
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            File source = screenshot.getScreenshotAs(OutputType.FILE);
+            File destination = new File(System.getProperty("user.dir") +
+                    "/resources/screenshots/" +
+                    java.time.LocalDate.now() + " " +
+                    testResult.getName() + ".png");
+            try {
+                FileHandler.copy(source, destination);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("Screenshot located at " + destination);
+        }
+    }
+}
